7f79b56fb1bb06131b77b393630eceeb88bb6eae
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2018-10-10 11:49:52 +0200

    reformatting and integrating licence

--- Lockin.py
+++ Lockin.py
@@ -3,70 +3,86 @@
 Created on Sat Apr 21 17:11:24 2018
 
 @author: GVolM
+
+    Copyright (C) 2018 Vladimir Grigorev, Steinn Ymir Agustsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 import Instrument
 import random
 import time
 
+
 class Lockin(Instrument.Instrument):
     def __init__(self):
-        
-        
-        self.configuration={'Sensitivity':0,'Time constant':0,'Reference source':0,
-                            'Frequency':1,'Reference trigger':0}
+        super(Lockin, self).__init__()
+
+        self.configuration = {'Sensitivity': 0, 'Time constant': 0, 'Reference source': 0,
+                              'Frequency': 1, 'Reference trigger': 0}
 
     def connect(self):
         print('Fake Lockin amplifier is connected')
-        
+
     def disconnect(self):
         print('Fake Lockin amplifier is disconnected')
-    
-    
-    def read_value(self,parameter):
+
+    def read_value(self, parameter):
         '''Reads measured value from lockin. Parametr is a string like in manual. 
         except Theta. Che the dictionary of parametrs for Output
         '''
-        Value=random.random() # returns value as a float 
-        print(parameter+' = '+str(Value)+' V')
+        Value = random.random()  # returns value as a float
+        print(parameter + ' = ' + str(Value) + ' V')
         time.sleep(0.1)
         return Value
-#%% set parameters
-        
+
+    # %% set parameters
+
     def set_sensetivity(self, sens):
-        self.configuration['Sensetivity']=sens
-        
+        self.configuration['Sensetivity'] = sens
+
     def set_time_constant(self, const):
-        self.configuration['Time constant']=const
-        
+        self.configuration['Time constant'] = const
+
     def set_frequency(self, freq):
-        self.configuration['Frequency']=freq
-    
+        self.configuration['Frequency'] = freq
+
     def set_reference_source(self, ref):
-        self.configuration['Reference source']=ref
-        
+        self.configuration['Reference source'] = ref
+
     def set_reference_trigger(self, reftrig):
-        self.configuration['Reference trigger']=reftrig
-        
-#%% get parameters       
-    
+        self.configuration['Reference trigger'] = reftrig
+
+    # %% get parameters
+
     def get_sensetivity(self):
         pass
-        
+
     def get_time_constant(self):
         pass
-        
+
     def get_frequency(self):
         pass
-    
+
     def get_reference_source(self):
         pass
-        
+
     def get_reference_trigger(self):
         pass
 
+    # %% Configuration of lockin functions
 
-#%% Configuration of lockin functions
-        
     def get_configuration(self):
         self.get_sensitivity()
         self.get_time_constant()
@@ -74,16 +90,15 @@ class Lockin(Instrument.Instrument):
         self.get_reference_source()
         self.get_reference_trigger()
 
-    
     def set_configuration(self):
         self.set_sensitivity(self.configuration['Sensitivity'])
         self.set_time_constant(self.configuration['Time constant'])
         self.set_frequency(self.configuration['Frequency'])
         self.set_reference_source(self.configuration['Reference source'])
         self.set_reference_trigger(self.configuration['Reference trigger'])
-        
+
     def saveConfiguration(self):
         pass
-    
+
     def laodConfiguration(self):
         pass
--- NewPortStage.py
+++ NewPortStage.py
@@ -10,86 +10,101 @@ Created on Wed Jul 19 15:58:15 2017
 Created on Tue May  9 14:06:55 2017
 
 @author: vgrigore
+
+    Copyright (C) 2018 Vladimir Grigorev, Steinn Ymir Agustsson
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import sys
-import clr 
+import clr
 import System
 import Stage
 import time
 
+
 class NewPortStage(Stage.Stage):
-    
+
     def __init__(self):
-        self.NETAssemblyPath=r'C:\Windows\Microsoft.NET\assembly\GAC_64\Newport.XPS.CommandInterface\v4.0_1.0.0.0__9a267756cf640dcf'
+        super(NewPortStage, self).__init__()
+        self.NETAssemblyPath = r'C:\Windows\Microsoft.NET\assembly\GAC_64\Newport.XPS.CommandInterface\v4.0_1.0.0.0__9a267756cf640dcf'
         sys.path.append(self.NETAssemblyPath)
         clr.AddReference("Newport.XPS.CommandInterface")
-        import CommandInterfaceXPS 
-        self.myXPS=CommandInterfaceXPS.XPS()
-        self.Address='192.168.254.254'
-        self.Port=5001
-        self.StageName="CykBlyat"
-        self.velocity=100
-        
-    def XPS_Open (self):
+        import CommandInterfaceXPS
+        self.myXPS = CommandInterfaceXPS.XPS()
+        self.Address = '192.168.254.254'
+        self.Port = 5001
+        self.StageName = "CykBlyat"
+        self.velocity = 100
+
+    def XPS_Open(self):
         # Create XPS interface
         # Open a socket
         timeout = 1000
         result = self.myXPS.OpenInstrument(self.Address, self.Port, timeout)
         if result == 0:
-            print( 'Open ', self.Address, ":", self.Port, " => Successful")
+            print('Open ', self.Address, ":", self.Port, " => Successful")
         else:
-            print ('Open ', self.Address, ":", self.Port, " => failure ", result)
-        
+            print('Open ', self.Address, ":", self.Port, " => failure ", result)
+
     def connect(self):
         self.XPS_Open()
-        self.myXPS.GroupKill(System.String(self.StageName),System.String(""))
-        self.myXPS.GroupInitialize(System.String(self.StageName),System.String(""))
-        self.myXPS.GroupHomeSearch(System.String(self.StageName),System.String(""))
-        self.myXPS.GroupMotionEnable(System.String(self.StageName),System.String(""))
+        self.myXPS.GroupKill(System.String(self.StageName), System.String(""))
+        self.myXPS.GroupInitialize(System.String(self.StageName), System.String(""))
+        self.myXPS.GroupHomeSearch(System.String(self.StageName), System.String(""))
+        self.myXPS.GroupMotionEnable(System.String(self.StageName), System.String(""))
         self.move_absolute(self.position_zero)
-    
-    
-    
+
     def move_absolute(self, new_position):
         '''Moves stage to the given position in range of +/- 150 mm '''
-        
-        time_to_sleep=(abs(self.position_current-new_position))/self.velocity
-        if (new_position<self.position_max)and(new_position<self.position_min):
-            self.myXPS.GroupMoveAbsolute(System.String(self.StageName),[System.Double(new_position)],System.Int32(1),System.String(""))
-            self.position_current=new_position
+
+        time_to_sleep = (abs(self.position_current - new_position)) / self.velocity
+        if (new_position < self.position_max) and (new_position < self.position_min):
+            self.myXPS.GroupMoveAbsolute(System.String(self.StageName), [System.Double(new_position)], System.Int32(1),
+                                         System.String(""))
+            self.position_current = new_position
             time.sleep(time_to_sleep)
-            print('Stage was moved to '+str(new_position))
+            print('Stage was moved to ' + str(new_position))
         else:
             print('position is out of range')
-        
-    def position_get(self):
-        pos=self.myXPS.GetCurrentPosition(System.Double(0),System.Double(0),System.Int32(1),System.Int32(1),System.Int32(1),System.Int32(1),System.String(self.StageName))
-        return pos
-    
-    def disconnect(self):
-        self.myXPS.GroupKill(System.String(self.StageName),System.String(""))
-        print('Stage has been disconnected')
-        
-    
-        
-#%%
 
-    
-    def XPS_GetControllerVersion (self, myXPS, flag):
-        result, version, errString = self.myXPS.FirmwareVersionGet(System.String(""),System.String(""))
+    def position_get(self):
+        pos = self.myXPS.GetCurrentPosition(System.Double(0), System.Double(0), System.Int32(1), System.Int32(1),
+                                            System.Int32(1), System.Int32(1), System.String(self.StageName))
+        return pos
+
+    def disconnect(self):
+        self.myXPS.GroupKill(System.String(self.StageName), System.String(""))
+        print('Stage has been disconnected')
+
+    # %%
+
+    def XPS_GetControllerVersion(self, myXPS, flag):
+        result, version, errString = self.myXPS.FirmwareVersionGet(System.String(""), System.String(""))
         if flag == 1:
             if result == 0:
                 print('XPS firmware version => ', version)
             else:
-                print('FirmwareVersionGet Error => ',errString)
+                print('FirmwareVersionGet Error => ', errString)
             return result, version
-        
-    def XPS_GetControllerState (self, myXPS, flag):
-        result, state, errString = self.myXPS.ControllerStatusGet(System.Int32(0),System.String(""))
+
+    def XPS_GetControllerState(self, myXPS, flag):
+        result, state, errString = self.myXPS.ControllerStatusGet(System.Int32(0), System.String(""))
         if flag == 1:
             if result == 0:
                 print('XPS controller state => ', state)
             else:
-                print('ControllerStatusGet Error => ',errString)
+                print('ControllerStatusGet Error => ', errString)
         return result, state

26d6a70526b5a0bb0e9269b26d8b304850d59c8c
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2018-10-29 18:35:28 +0100

    added worker class to core, WIP

--- measurement/core.py
+++ measurement/core.py
@@ -9,7 +9,10 @@ import time
 from PyQt5 import QtCore
 from utilities.qt import raise_Qerror
 from utilities.misc import nested_for
+from utilities.math import globalcounter
+
 import pandas as pd
+
 
 class Experiment(QtCore.QObject):
 
@@ -18,10 +21,10 @@ class Experiment(QtCore.QObject):
 
         This is initialized by creating adding all the passed arguments as instrument instances, and creates a
         list of names of them in self.instrument_list. Then, all methods which can be used to measure some quantity
-        and all parameters which can be set for each instruments are collected in self.measurables and self.parameters
+        and all parameters which can be set for each instruments are collected in self.measurable_methods and self.parameters
         respectively.
 
-        self.measurables is therefore a list of methods which can be directly called. These methods must have a test on
+        self.measurable_methods is therefore a list of methods which can be directly called. These methods must have a test on
         the parent instrument being connected, or might fail.
         self. parameters is a dictionary, where keys are the names of the instruments to which they belong. Each value
         is then a dictionary containing the name of the parameter as keys and a pointer to the parameter instance in the
@@ -86,7 +89,6 @@ class Experiment(QtCore.QObject):
             for name in self.instrument_list:
                 print('{}: type:{}'.format(name, type(getattr(self, name))))
 
-
     def connect_all(self, *args):
         """ Connect to the instruments.
 
@@ -135,7 +137,7 @@ class Experiment(QtCore.QObject):
 
 
         :parameters:
-            measurables:
+            measurable_methods:
                 measuring methods to be performed at each iteration
 
             parameters: list of methods
@@ -145,26 +147,180 @@ class Experiment(QtCore.QObject):
                 the values over which to iterate the parameters.
                 example: ([1,2,3,4,5,6,7,8,9,10],[4,8,12,20,30,50,100,200,300])
         """
+
         def measurement_step(indexes, parameter_methods, values):
             """ set the correct parameters and start a measurement step"""
-            result = []#pd.DataFrame()
-            for i,func in enumerate(parameter_methods):
-                func(values[i][indexes[i]]) # set all parameters
+            result = []  # pd.DataFrame()
+            for i, func in enumerate(parameter_methods):
+                func(values[i][indexes[i]])  # set all parameters
 
             for func in measurables:
                 # out = func
-                result.append(func)# todo: replace with actual measurement routine (worker)
+                result.append(func)  # todo: replace with actual measurement routine (worker)
             return result
 
-        ranges=[]
+        ranges = []
         for iter_vals in values:
-            ranges.append((0,len(iter_vals)))
+            ranges.append((0, len(iter_vals)))
         ranges = tuple(ranges)
 
-        nested_for(ranges,measurement_step,parameter_methods, values)
+        nested_for(ranges, measurement_step, parameter_methods, values)
 
 
 class Worker(QtCore.QObject):
+    """ This class manages a measurement session.
+
+    An instance of this class is designed to be sent to a new thread, which will
+    output signals to enable control from the original thread (GUI or not).
+
+    Signals:
+        newData (list): emitted at each new data aquisition, by the method measure()
+            Emitted data is a list containing the results from each measurable method used.
+        finished: emitted when the whole measurement procedure is complete.
+        cycleEnd : emitted at each end of a measurement cycle TODO: implement, or delete
+
+    """
+    finished = QtCore.pyqtSignal()
+    newData = QtCore.pyqtSignal(list)
+    newCycle = QtCore.pyqtSignal(int)
+    STATE_VALUES = ['loading', 'idle', 'running', 'error']
+
+    def __init__(self, file, measurable_methods, measurable_values, parameter_methods, parameter_values):
+        """ Initialize with measurement and parameter setting methods and values.
+
+
+
+        Args:
+            file (str): path to file where to save data
+            measurable_methods (:obj:`list` of :obj:`method`): functions to run to
+                gather data at each measurement step
+            measurable_values (:obj:`list` of :obj:`list` of :obj:`float`): values to be
+                passed to the measurable methods.
+            parameter_methods (:obj:`list` of :obj:`method`): methods to be called
+                to change parameters
+            parameter_values (:obj:`list` of :obj:`list` of :obj:`float`): values to be
+                passed to the parameter setting methods.
+        """
+        super().__init__()
+        self.save_file = file
+        self.measurable_methods = measurable_methods
+        self.measurable_values = measurable_values
+        self.parameter_methods = parameter_methods
+        self.parameter_values = parameter_values
+
+
+        self.__prev_index = None
+
+    def work(self):
+        """ Create a loop for each parameter this worker should iterate on.
+
+        The loop will be equivalent to a nested series of for loops, one for
+        each list of values in self.parameter_values. The first method will be
+        the innermost cycle.
+
+        :example:
+        with
+        parameter_methods = [set_temperature, set_polarization]
+        parameter_values = [[10,20,30,40,50],[0,1,2,3]]
+        will generate the equivalent of this loop:
+        for temperature_values in parameter_values[0]:
+            for polarization_values in parameter_values[1]:
+                parameter_methods[0](temperature_values)
+                parameter_methods[1](polarization_values)
+                measure()
+
+
+        """
+        def measurement_step(indexes, parameter_methods, values):
+            """ set the correct parameters and start a measurement step"""
+            # TODO: implement global counter, M is missing
+            #self.newCycle.emit(globalcounter(indexes,M)) # emit signal with global counter number
+            if self.__prev_index is None: # at first iteration, start keeping track of indexes
+                self.__prev_index = []
+                for i in range(len(indexes)):
+                    self.__prev_index.append(0)
+
+            for i, idx in enumerate(indexes): # check which parameters need to be changed at this iteration
+                if idx != self.__prev_index[i]:
+                    parameter_methods[i](values[indexes[i]]) # set only the parameters which change at this cycle
+
+            for i, func in enumerate(parameter_methods):
+
+                func(values[i][indexes[i]])  # set all parameters
+
+            self.measure()
+
+        ranges = []
+        for iter_vals in self.parameter_values:
+            ranges.append((0, len(iter_vals)))
+        ranges = tuple(ranges)
+
+        nested_for(ranges, measurement_step, self.parameter_methods, self.parameter_values)
+        self.finished.emit()
+
+
+    def measure(self):
+        """ Perform a single point measurement.
+
+        This uses all methods defined in self.measurable_methods to record all data desired.
+
+        """
+        result = []
+        for i, mtd in enumerate(self.measurable_methods):
+            result.append(mtd(self.measurable_values[i]))
+        self.newData.emit(result)
+
+
+
+    @QtCore.pyqtSlot()
+    def kill_worker(self):
+        """ Safely kill the thread by closing connection to all insturments."""
+        for instrument in self.requiredInstruments:
+            try:
+                getattr(self, instrument).close()
+                print('closed connection to {}'.format(instrument))
+            except AttributeError:
+                pass
+            except Exception as e:
+                raise_Qerror('killing {}'.format(instrument), e, popup=False)
+
+    @property
+    def state(self):
+        """ Get the current worker state.
+
+        Allowed States:
+            loading: worker is setting up initial conditions.
+            idle: ready to run, awaiting starting condition.
+            running: obvious.
+            error: worker stuck because of error or something.
+        """
+        return self.state
+
+    @state.setter
+    def state(self, value):
+        """ set new state flag.
+
+        Sets a new state, emits a signal with the changed state value.
+        """
+        assert value in self.STATE_VALUES, 'invalid status: {}'.format(value)
+        self.__state = value
+        self.valueChanged.emit(value)
+
+    @QtCore.pyqtSlot(bool)
+    def should_stop(self, flag):
+        """ Soft stop, for interrupting at the end of the current cycle.
+
+        For hard stop, use kill_worker slot.
+        Args:
+            flag (bool): True sets the worker to stop after current cycle.
+                False turns this option off.
+        """
+        self.shouldStop = flag
+
+
+
+
+class OLD_Worker(QtCore.QObject):
     """ Parent class for all workers.
 
     This class is to be launched and assigned to a thread.
--- utilities/math.py
+++ utilities/math.py
@@ -14,6 +14,13 @@ def gaussian(x, mu, sig):
     return np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.)))
 
 
+def globalcounter(idx,M):
+    counterlist = idx[::-1]
+    for i in range(len(counterlist)):
+        counterlist[i] = counterlist[i]*np.prod(M[:i])
+    return int(np.sum(counterlist))
+
+
 def main():
     pass
 

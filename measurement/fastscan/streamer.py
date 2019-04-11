db99d1f9673f5dd7f3bcbc83ebb197bd97772405
Unknown <sagustss@uni-mainz.de>
2019-04-11 15:39:54 +0200

    new triggered acquisition method

--- measurement/fastscan/streamer.py
+++ measurement/fastscan/streamer.py
@@ -47,7 +47,7 @@ class FastScanStreamer(QtCore.QObject):
     newData = QtCore.pyqtSignal(np.ndarray)
     error = QtCore.pyqtSignal(Exception)
 
-    def __init__(self, n_samples, iterations=None, simulate=False, dark_control=True):
+    def __init__(self, n_samples, iterations=None, dark_control=True, simulate=False):
         super().__init__()
         self.logger = logging.getLogger('{}.FastScanStreamer'.format(__name__))
         self.logger.info('Created FastScanStreamer')
@@ -55,6 +55,7 @@ class FastScanStreamer(QtCore.QObject):
         self.n_samples = n_samples
         self.iterations = iterations
         self.data = np.zeros((3, n_samples))
+        self.acquisition_mode = parse_setting('fastscan','acquisition_mode')
         self.simulate = simulate
         self.dark_control = dark_control
         self.should_stop = True
@@ -67,55 +68,87 @@ class FastScanStreamer(QtCore.QObject):
     @QtCore.pyqtSlot()
     def start_acquisition(self):
         if self.simulate:
-            self.logger.info('Started streamer simulation')
-            self.start_simulated_acquisition()
+            self.logger.info('Started streamer simulation in {} mode'.format(self.acquisition_mode))
+            self.measure_simulated()
         else:
-            try:
-                self.logger.info('Started NI FastScanStreamer')
-                with nidaqmx.Task() as task:
+            if self.acquisition_mode == 'continuous':
+                self.logger.info('Started NI continuous Streamer ')
+                self.measure_continuous()
 
-                    self.reader = stream_readers.AnalogMultiChannelReader(task.in_stream)
-                    self.logger.debug('reader initialized')
-                    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")  # shaker position chanel
-                    task.ai_channels.add_ai_voltage_chan("Dev1/ai1")  # signal chanel
-                    task.ai_channels.add_ai_voltage_chan("Dev1/ai2")  # dark control chanel
-                    self.logger.debug('added 3 tasks')
-                    task.timing.cfg_samp_clk_timing(1000000, source="/Dev1/PFI0",
-                                                    active_edge=Edge.RISING,
-                                                    sample_mode=AcquisitionType.CONTINUOUS)
-                    task.start()
-                    self.logger.info('FastScanStreamer taks started')
-
-                    self.should_stop = False
-                    i = 0
-                    while True:
-                        i += 1
-                        self.logger.debug('measuring cycle {}'.format(i))
-                        self.measure()
-                        if self.iterations is not None and i >= self.iterations:
-                            self.should_stop = True
-                        if self.should_stop:
-                            self.logger.warning('Acquisition stopped.')
-                            self.finished.emit()
-                            break
-
-            except Exception as e:
-                self.logger.warning('Error while starting streamer: \n{}'.format(e))
-                self.error.emit(e)
+            elif self.acquisition_mode == 'triggered':
+                self.logger.info('Started NI triggered Streamer ')
+                self.measure_triggered()
 
     @QtCore.pyqtSlot()
     def stop_acquisition(self):
         self.logger.info('FastScanStreamer thread stopping.')
         self.should_stop = True
 
-    def measure(self):
-        self.reader.read_many_sample(self.data, number_of_samples_per_channel=self.n_samples)
-        self.logger.debug('Recieved data from NI card: mean axis 0 = {}'.format(self.data[0].mean()))
-        self.newData.emit(self.data)
+    def measure_continuous(self):
+        try:
+            with nidaqmx.Task() as task:
 
-    def start_simulated_acquisition(self):
-        self.logger.debug('Starting simulation')
+                self.reader = stream_readers.AnalogMultiChannelReader(task.in_stream)
+                self.logger.debug('reader initialized')
+                task.ai_channels.add_ai_voltage_chan("Dev1/ai0")  # shaker position chanel
+                task.ai_channels.add_ai_voltage_chan("Dev1/ai1")  # signal chanel
+                task.ai_channels.add_ai_voltage_chan("Dev1/ai2")  # dark control chanel
+                self.logger.debug('added 3 tasks')
+                task.timing.cfg_samp_clk_timing(1000000, source="/Dev1/PFI0",
+                                                active_edge=Edge.RISING,
+                                                sample_mode=AcquisitionType.CONTINUOUS)
+                task.start()
+                self.logger.info('FastScanStreamer taks started')
 
+                self.should_stop = False
+                i = 0
+                while True:
+                    i += 1
+                    self.logger.debug('measuring cycle {}'.format(i))
+                    self.reader.read_many_sample(self.data, number_of_samples_per_channel=self.n_samples)
+                    self.logger.debug('Recieved data from NI card: mean axis 0 = {}'.format(self.data[0].mean()))
+                    self.newData.emit(self.data)
+                    if self.iterations is not None and i >= self.iterations:
+                        self.should_stop = True
+                    if self.should_stop:
+                        self.logger.warning('Acquisition stopped.')
+                        self.finished.emit()
+                        break
+
+        except Exception as e:
+            self.logger.warning('Error while starting streamer: \n{}'.format(e))
+            self.error.emit(e)
+
+    def measure_triggered(self):
+        with nidaqmx.Task() as task:
+            task.ai_channels.add_ai_voltage_chan("Dev1/ai0")  # shaker position chanel
+            task.ai_channels.add_ai_voltage_chan("Dev1/ai1")  # signal chanel
+            task.ai_channels.add_ai_voltage_chan("Dev1/ai2")  # dark control chanel
+
+            task.triggers.start_trigger.cfg_dig_edge_start_trig(trigger_source="/Dev1/PFI1",
+                                                                trigger_edge=Edge.RISING)
+            task.timing.cfg_samp_clk_timing(100000, samps_per_chan=self.n_samples,
+                                            source="/Dev1/PFI0",
+                                            active_edge=Edge.RISING,
+                                            sample_mode=AcquisitionType.FINITE)  # external clock chanel
+
+            self.should_stop = False
+            i = 0
+            while True:
+                i += 1
+                self.logger.debug('measuring cycle {}'.format(i))
+                self.data = task.read(number_of_samples_per_channel=self.n_samples)
+
+                self.newData.emit(self.data)
+
+                if self.iterations is not None and i >= self.iterations:
+                    self.should_stop = True
+                if self.should_stop:
+                    self.logger.warning('Acquisition stopped.')
+                    self.finished.emit()
+                    break
+
+    def measure_simulated(self):
         self.should_stop = False
 
         if self.iterations is None:
@@ -125,7 +158,11 @@ class FastScanStreamer(QtCore.QObject):
                 self.logger.debug('simulating measurement cycle #{}'.format(i))
                 t0 = time.time()
 
-                self.data = simulate_measure(self.data,function='sech2_fwhm', args=[.5, -2, .085, 1], amplitude=50)
+                self.data = simulate_measure(self.data,
+                                             function='sech2_fwhm',
+                                             args=[.5, -2, .085, 1],
+                                             amplitude=50,
+                                             mode=self.acquisition_mode)
                 dt = time.time() - t0
                 time.sleep(max(self.n_samples / 273000 - dt, 0))
                 self.newData.emit(self.data)
@@ -157,7 +194,9 @@ class FastScanStreamer(QtCore.QObject):
                                                       self.data.shape))
 
 
-def simulate_measure(data, function='sech2_fwhm', args=[.5, -2, .085, 1], amplitude=10):
+
+
+def simulate_measure(data, function='sech2_fwhm', args=[.5, -2, .085, 1], amplitude=10, mode='triggered'):
     args_ = args[:]
     step = parse_setting('fastscan', 'shaker_position_step')
     ps_per_step = parse_setting('fastscan', 'shaker_ps_per_step')  # ADC step size - corresponds to 25fs
@@ -191,8 +230,10 @@ def simulate_measure(data, function='sech2_fwhm', args=[.5, -2, .085, 1], amplit
     #########
     n = np.arange(len(data[0]))
     noise = np.random.rand(len(n))
-    phase = noise[0] * 2 * np.pi
-
+    if mode == 'continuous':
+        phase = noise[0] * 2 * np.pi
+    else:
+        phase = 0
     amplitude = amplitude * step / ps_per_step * (1 + .02 * np.random.uniform(-1, 1))
 
     data[0, :] = np.cos(2 * np.pi * n / 30000 + phase) * amplitude / 2  # in volt
